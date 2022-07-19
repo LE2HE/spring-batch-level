974bd26f0b4c4e9956b650883413371db235c447
LE2HE <leehe4352@gamil.com>
2022-07-19 21:03:21 +0900

    Setup : Spring Batch Project
    자바 스프링 배치 프로젝트 (Gradle)

---
+++ src/main/java/fastcampus/spring/batch/springbatchlevel/SpringBatchLevelApplication.java
@@ -0,0 +1,13 @@
+package fastcampus.spring.batch.springbatchlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchLevelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fastcampus/spring/batch/springbatchlevel/SpringBatchLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.spring.batch.springbatchlevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
