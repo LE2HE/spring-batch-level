ab008ea490147fd629324c5b62ef31facc2b20e5
LE2HE <leehe4352@gamil.com>
2022-07-20 22:51:19 +0900

    Test #9 : UserConfiguration 테스트 구현
    테스트 구현
    - findAllByUpdate 추가
    - TestConfiguration 생성
    - userSaveStep 테스트 구현
    - userLevelUpStep 테스트 구현

--- src/main/java/fastcampus/spring/batch/springbatchlevel/UserRepository.java
+++ src/main/java/fastcampus/spring/batch/springbatchlevel/UserRepository.java
@@ -2,5 +2,9 @@ package fastcampus.spring.batch.springbatchlevel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Collection<Object> findAllByUpdateDate(LocalDate updateDate);
 }
