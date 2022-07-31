7785a2f6824fdda926cb6c4676e4178c8d2f5932
LE2HE <leehe4352@gamil.com>
2022-07-31 22:58:38 +0900

    Add #21 : TaskExecutor 빈 생성
    안전하게 종료할 수 있도록 System.exit 추가
    TaskExecutor 빈 생성
    - ThreadPoolTaskExecutor 사용

--- src/main/java/fastcampus/spring/batch/springbatchlevel/SpringBatchLevelApplication.java
+++ src/main/java/fastcampus/spring/batch/springbatchlevel/SpringBatchLevelApplication.java
@@ -3,13 +3,28 @@ package fastcampus.spring.batch.springbatchlevel;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchLevelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchLevelApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchLevelApplication.class, args)));
+	}
+
+	@Bean
+	@Primary
+	TaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(10);
+		taskExecutor.setMaxPoolSize(20);
+		taskExecutor.setThreadNamePrefix("batch-thread-");
+		taskExecutor.initialize();
+		return taskExecutor;
 	}
 
 }
